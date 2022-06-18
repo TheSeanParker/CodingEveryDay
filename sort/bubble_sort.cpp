f4d9a19764eb4d9b2dc769104c9abbddedaa3a29
Sean <66415023+TheSeanParker@users.noreply.github.com>
2022-06-18 11:00:56 +0800

    Add files via upload

--- sort/bubble_sort.cpp
+++ sort/bubble_sort.cpp
@@ -26,19 +26,11 @@ void bubble(vector<int>& a,int count)
 
 int main(int argc, char const *argv[])
 {
-
     vector<int> original;//容器是动态数据结构，可以一开始不声明容器大小。但是数组必须要
-    cout<<"请输入数据:"<<endl;
-    int str;
-    std::cin>>str;//?r如何得到输入的长度，并进行计算？
-    for (size_t i = 0; i < str.size(); i++)
-    {
-       original[i]=str[i];
-    }
+    original={1,2,3,4,6};
+
     int num=5;
     bubble(original, num);
     std::cout<<"original[2]="<<original[2]<<std::endl;
-    std::cout<<"original[3]="<<original[3]<<std::endl;
-    //cout<<"bubble size="<<sizeof(original)/sizeof(int)<<endl;//有没有其他方法获取数组的长度
     return 0;
 }
