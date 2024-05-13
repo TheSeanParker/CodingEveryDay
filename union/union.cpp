c49d13028911b45b42c0fda0315f4b544a01fd2f
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-05-13 19:46:16 +0800

    Add files via upload

---
+++ union/union.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+union   
+{ 
+	float   fuel_load; 
+	char a[3];
+          // char b[9];
+	int   pallets; 
+}fighter; 
+
+struct   aircraft 
+{ 
+          int   wingspan; //4
+          int   passengers; //4
+          union   
+          { 
+                    float   fuel_load; //4
+                    float   bomb_load; //4
+                    long   pallets; //4
+          }; 
+}test2; 
+
+union   
+{ 
+          struct 
+          {
+             float   fuel_load; //4
+          //    float   bomb_load; //4
+          //    float   pallets; //4
+          };
+          float data[4];
+}test3; 
+
+// PCL中的PointXYZ数据类型
+union
+{
+  float data[4];
+  struct
+  {
+    float x;
+    float y;
+    float z;
+  };
+}PointXYZ;
+int main(int argc, char const *argv[])
+{
+          /* code */
+          std::cout<<"size="<<sizeof(fighter)<<std::endl;
+          std::cout<<"test2="<<sizeof(test2)<<std::endl;
+          std::cout<<"test3="<<sizeof(test3)<<std::endl;
+          return 0;
+}
