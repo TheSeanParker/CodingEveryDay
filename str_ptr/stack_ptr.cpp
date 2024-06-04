3ea10335cbaf2c2917d13700f543061732690e3d
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-06-04 08:47:14 +0800

    Add files via upload
    不允许对空指针,解引用

--- str_ptr/stack_ptr.cpp
+++ str_ptr/stack_ptr.cpp
@@ -6,6 +6,11 @@ int main(int argc, char const *argv[])
           int b=2;
           int*a=&b;
           std::cout<<"*a+1="<<*a+1<<std::endl;
-          delete a;//栈指针不允许被删除,所以报错!
+          int* ptr=new int;
+          // ptr=nullptr; //不允许对空指针进行解引用
+          int c=30;
+          *ptr=c;
+          // delete a;//栈指针不允许被删除,所以报错!
+          std::cout<<"*ptr="<<*ptr<<std::endl;
           return 0;
 }
