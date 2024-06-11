4d8cd5a6200513e05169139bca400f2c8d4d5f00
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-06-11 20:11:10 +0800

    Add files via upload

---
+++ string/string.cpp
@@ -0,0 +1,10 @@
+#include<string>
+#include<iostream>
+
+int main(int argc, char const *argv[])
+{
+          std::string saveMapDirectory("/home/deploy/桌面/demo/C++/string/");
+          std::string name_A="Corner_Map/"+saveMapDirectory+"corner.pcd"+"demo"+"test";
+          std::cout<<"="<<name_A<<std::endl;
+          return 0;
+}
