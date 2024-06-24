0f838f3bb5f08d2587e338a4283705c35d7c78be
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-06-24 21:05:49 +0800

    Add files via upload

--- string/const_char.cpp
+++ string/const_char.cpp
@@ -4,10 +4,16 @@ void SetLogDestination(const char* base_filename)
 {
           std::cout<<"right"<<std::endl;
 }
+void SetLogDestination_string(std::string&  base_filename)
+{
+          std::cout<<"day"<<std::endl;
+}
 int main(int argc, char const *argv[])
 {
           std::string a="test";
+          const char b[]="set";
           // SetLogDestination(a);
           SetLogDestination(a.c_str());
+          SetLogDestination_string(b);
           return 0;
 }
