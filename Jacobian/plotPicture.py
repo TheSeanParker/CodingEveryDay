769e9619bc23e5e88db9529edbdc3d89cf7a503e
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-09-07 14:03:36 +0800

    Delete Jacobian directory

--- Jacobian/plotPicture.py
+++
@@ -1,7 +0,0 @@
-# coding:utf-8
-import matplotlib.pyplot as plt
-import numpy as np
-import math
-x = np.arange(-5, 5, 0.033)        #从1到9，间隔1取点
-plt.plot(x,[(math.pow(i,-1)*(-1)+math.pow(i,1)/(-2))for i in x],"gs-", label="x*x*x") #"r"为红色, "s"为方块, "-"为实线
-plt.show()
