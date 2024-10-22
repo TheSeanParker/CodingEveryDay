626625d126ff6a2995219aa82b3f6ffa1051634f
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-10-22 11:53:03 +0800

    Add files via upload

--- PCL_Example/PCDWriter/src/main.cpp
+++ PCL_Example/PCDWriter/src/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    string ROOT_DIR_="/home/deploy/PCL_IO/PCDWriter/src";
+    string ROOT_DIR_="/home/deploy/PCL_IO/";
      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
         for (size_t i = 1; i <= 4; i++)
         {
@@ -19,7 +19,12 @@ int main(int argc, char const *argv[])
         string file_name = string("GlobalMap.pcd");
         string all_points_dir(string(string(ROOT_DIR_) + "/") + file_name);
         pcl::PCDWriter pcd_writer;
+        std::cout<<" PCDWriter cloud->size()="<<cloud->size()<<std::endl;
         std::cout << "current Large scan saved to /PCD/" << file_name << endl;
         pcd_writer.writeBinary(all_points_dir, *cloud);
+        std::cout<<" savePCDFile cloud->size()="<<cloud->size()<<std::endl;
+        string file_name_savePCDFile = string("demo.pcd");
+        string all_points_savePCDFile(string(string(ROOT_DIR_) + "/") + file_name_savePCDFile);
+        pcl::io::savePCDFile(all_points_savePCDFile,*cloud);
     return 0;
 }
