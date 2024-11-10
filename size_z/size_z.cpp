9a8a35f96e08a4aa6b31cc2cc6e69b1196528b6a
Sean <66415023+TheSeanParker@users.noreply.github.com>
2024-11-10 11:14:49 +0800

    Add files via upload

---
+++ size_z/size_z.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+          size_t a=1;
+          std::cout<<"a-1="<<a-1<<std::endl;
+          std::cout<<"a-2="<<a-2<<std::endl;
+          size_t b=9;
+          std::cout<<"a%4="<<a%4<<std::endl;
+          std::cout<<"float(a/4)="<<float(a)/4<<std::endl;
+          return 0;
+}
